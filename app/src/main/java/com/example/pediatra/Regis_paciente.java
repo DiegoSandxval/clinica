887cf5d87b15ffd28f2442d3837ed4a9412052f5
Diego Sandoval <sandovaldiego21@gmail.com>
2022-02-26 01:30:15 -0600

    so

---
+++ app/src/main/java/com/example/pediatra/Regis_paciente.java
@@ -0,0 +1,14 @@
+package com.example.pediatra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Regis_paciente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regis_paciente);
+    }
+}
